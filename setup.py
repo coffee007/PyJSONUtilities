ffd0947e2802b2996a3a0ae0befc81784f9d67c2
ad007 <monprojectsemail@gmail.com>
2021-05-03 14:07:37 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='pyjsonutils',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Some basic functions used often with JSON files',
+    author='Me',
+    license='MIT',
+)
---
+++ tests/test.py
@@ -0,0 +1,4 @@
+from pyjsonutils.main import open_file_as_dict
+x = open_file_as_dict("test.json")
+
+print(x)
